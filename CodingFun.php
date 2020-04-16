a52eca7d73728a79e5a417d7ef967897259e47ea
Devtest TestButler <testbutler@devtest.net>
2020-04-16 15:01:10 +0000

    Attempt 1

--- CodingFun.php
+++ CodingFun.php
@@ -5,12 +5,33 @@ class CodingFun
 
     public function getFibonacciSequence($resultArraySize)
     {
-        // 
+        $input = $resultArraySize;
+        $array = [];
+        for ($counter =0; $counter < $input; $counter++) {
+         	$array[$counter] = $this->test($counter);
+        }
+   		  return $array;
     }
+  
+    public function test ($n)
+    {
+    	if($n ==0) {
+			  return 0;
+		  }
 
+  		if($n == 1) {
+  			return 1;
+  		}		
+
+  		if($n != 0 OR $n != 1) {
+  			return ($this->test($n - 1) + $this->test($n-2));
+  		}
+    }
+    
     public function getCustomerBalances($customerTransactions)
     {
-        //
+        ksort($customerTransactions);
+        return $customerTransactions;
     }
 
     public function getLotteryWinnings($totalWinnings, $winningNumber, $players)
