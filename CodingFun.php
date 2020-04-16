1bb13a50c864de7f4783755c2cee384a06aac123
Devtest TestButler <testbutler@devtest.net>
2020-04-16 14:10:05 +0000

    Initial Commit

---
+++ CodingFun.php
@@ -0,0 +1,20 @@
+<?php
+
+class CodingFun
+{
+
+    public function getFibonacciSequence($resultArraySize)
+    {
+        // 
+    }
+
+    public function getCustomerBalances($customerTransactions)
+    {
+        //
+    }
+
+    public function getLotteryWinnings($totalWinnings, $winningNumber, $players)
+    {
+        //
+    }
+}
